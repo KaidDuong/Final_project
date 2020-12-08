b8a53d9be73cf670bbc6536bc352fd8cc901891c
KaidDuong <cooleen.cl@gmail.com>
2020-12-08 18:14:09 +0700

    deploy

---
+++ app/digits/models.py
@@ -0,0 +1,20 @@
+# # -*- encoding: utf-8 -*-
+# """
+# License: MIT
+# Copyright (c) 2019 - present AppSeed.us
+# """
+# from sqlalchemy import Binary, Column, Integer, String
+# # import uuid
+# from app import db
+
+# class HandDigits(db.Model):
+
+#     __tablename__ = 'HandDigits'
+
+#     # id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
+#     path = Column(String, unique=True)
+#     createdOn = Column(String)
+#     createdBy = Column(String)
+#     score = Column(Float)
+#     ouptput = Column(String)
+
